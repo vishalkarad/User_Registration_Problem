d57cb8c0afd3aac1ddc2db3329dbef97829dab8b
Your Name <you@example.com>
2020-03-04 11:07:35 +0530

    Added : Check mobile number pattern

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-#firstName="^[A-Z][a-zA-Z]{2,}$"
+firstName="^[A-Z][a-zA-Z]{2,}$"
 valiedEmail="^[a-zA-Z]{1,}([.]?[a-zA-Z]{1,})?[@]{1}[a-zA-Z]{1,}[.]{1}[a-z]{2}([.]?[a-z]{2})?$"
+moPattern="^[0-9]{2}[ ]{1}[0-9]{10}"
 
 #read -p "Enter a First name " firstname
 #read -p "Enter a Second name " lastname
-read -p "Enter a email " email
+#read -p "Enter a email " email
+read -p "Enter a Mobile Number" mobileNumber
 # check patter match or not
 function checkPattern()
 { 
 	if [[ $1 =~ $2 ]]
 	then
-		printf "$1 pattern is match\n"
+		printf "Valied\n"
 	else
-		printf "$1 Entern valied  Pattern\n"
+		printf "Invalied\n"
 	fi
+}
+
+# check mobile pattern
+function IntegerPattern()
+{ 
+   if [[ $mobileNumber =~ $moPattern ]]
+   then
+      printf "Valied\n"
+   else
+      printf "Invalied\n"
+   fi
 }
 #checkPattern $firstname $firstName
 #checkPattern $lastname $firstName
-checkPattern  $email $valiedEmail
+#checkPattern  $email $valiedEmail
+IntegerPattern
