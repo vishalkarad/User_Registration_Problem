8cf4520bbda2594689c8540ea188cda855ff718d
Your Name <you@example.com>
2020-03-04 15:52:12 +0530

    Added : At List One Capital Later in password and minimum length is 8

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -3,12 +3,24 @@
 firstName="^[A-Z][a-zA-Z]{2,}$"
 valiedEmail="^[a-zA-Z]{1,}([.]?[a-zA-Z]{1,})?[@]{1}[a-zA-Z]{1,}[.]{1}[a-z]{2}([.]?[a-z]{2})?$"
 moPattern="^[0-9]{2}[ ]{1}[0-9]{10}"
-passwordPattern="[a-zA-Z]{8,}$"
-read -p "Enter a First name " firstname
-read -p "Enter a Second name " lastname
-read -p "Enter a email " email
-read -p "Enter a Mobile Number" mobileNumber
+passWordLength={8,}
+passwordPattern="([A-Z][a-z]*)|([A-Z][0-9]*)|([A-Z][a-z0-9]*)$"
+
+#read -p "Enter a First name " firstname
+#read -p "Enter a Second name " lastname
+#read -p "Enter a email " email
+#read -p "Enter a Mobile Number" mobileNumber
 read -p "Enter a Password " password
+
+function checkPassWord()
+{ 
+   if [[ ${#password} -gt 8 && $password =~ $passwordPattern ]]
+   then
+      printf "Valied\n"
+   else
+      printf "Invalied\n"
+   fi
+}
 
 
 # check patter match or not
@@ -32,8 +44,10 @@ function CheckMobilePattern()
       printf "Invalied\n"
    fi
 }
-checkPattern $firstname $firstName
-checkPattern $lastname $firstName
-checkPattern  $email $valiedEmail
-CheckMobilePattern
-checkPattern $password $passwordPattern
+
+#checkPattern $firstname $firstName
+#checkPattern $lastname $firstName
+#checkPattern  $email $valiedEmail
+#CheckMobilePattern
+#checkPattern $password $passwordPattern
+checkPassWord
