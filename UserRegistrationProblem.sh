0558cab68ff1de9e6c4c89a5f575f4d320a006dd
Your Name <you@example.com>
2020-03-05 10:09:17 +0530

    Refactor : Change the password logic

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -3,14 +3,12 @@
 firstName="^[A-Z][a-zA-Z]{2,}$"
 valiedEmail="^[a-zA-Z]{1,}([.]?[a-zA-Z]{1,})?[@]{1}[a-zA-Z]{1,}[.]{1}[a-z]{2}([.]?[a-z]{2})?$"
 moPattern="^[0-9]{2}[ ]{1}[0-9]{10}"
-passWordLength={8,}
-#passwordPattern="([A-Z][a-z]*)|([A-Z][0-9]*)|([A-Z][a-z0-9]*)$"
+passwordPattern="^([a-zA-Z]*[0-9]+[a-z0-9]*[A-Z])|([A-Z][a-z0-9]*+[0-9][a-zA-Z]*)$"
 
-passwordPattern="^([a-zA-Z0-9]*[A-Z]+[a-zA-Z0-9]*[0-9]+[a-zA-Z0-9]*)+$"
-#read -p "Enter a First name " firstname
-#read -p "Enter a Second name " lastname
-#read -p "Enter a email " email
-#read -p "Enter a Mobile Number" mobileNumber
+read -p "Enter a First name " firstname
+read -p "Enter a Second name " lastname
+read -p "Enter a email " email
+read -p "Enter a Mobile Number" mobileNumber
 read -p "Enter a Password " password
 
 function checkPassWord()
@@ -46,9 +44,8 @@ function CheckMobilePattern()
    fi
 }
 
-#checkPattern $firstname $firstName
-#checkPattern $lastname $firstName
-#checkPattern  $email $valiedEmail
-#CheckMobilePattern
-#checkPattern $password $passwordPattern
+checkPattern $firstname $firstName
+checkPattern $lastname $firstName
+checkPattern  $email $valiedEmail
+CheckMobilePattern
 checkPassWord
